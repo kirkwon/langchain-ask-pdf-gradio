9b68dc45fe63a412beac5d33684ef2d3f2b4ccf3
Kirk Won <kirkwon@gmail.com>
2023-05-06 01:50:48 -0700

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
-# langchain-ask-pdf-gradio
+﻿# langchain-ask-pdf-gradio
+
+## Why
+Using langchain gets LLMs to pay attention to your content.
+
+## How
 Python project using a gradio interface to query an online pdf document using langchain
+
+## Quickstart
+Using a machine with python installed
+Clone or download this repo
+Make sure all the python packages in requirements.txt file are installed
+Rename the .env-template file to .env 
+Edit the .env file entering your OpenAI GPT API key (see below for details) after the equals sign on the first line
+
+Run the application with the following command:
+```
+python app.py
+```
+This will output a url which can be used by a browser.
+Entering a question and a url to a PDF available online should get you a syntactic-well-formed  semantic-related answer
+
+## OpenAI API key
+This can be found here:
+https://platform.openai.com/login
+
+After login or sign up you can find or create a new key under your account menu inn the upper right corner of the navigation bar. In the menu select "View API Key." This will redirect you to a screen where you can click on the "Create new secret key button." It is strongly recommended you create a new key for each application for which you use OpenAI GPT.
---
+++ app.py
@@ -0,0 +1,59 @@
+from dotenv import load_dotenv
+import gradio as gr
+import io
+import requests
+from PyPDF2 import PdfReader
+from langchain.text_splitter import CharacterTextSplitter
+from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain.vectorstores import FAISS
+from langchain.chains.question_answering import load_qa_chain
+from langchain.llms import OpenAI
+from langchain.callbacks import get_openai_callback
+
+# https://stackoverflow.com/questions/9751197/opening-pdf-urls-with-pypdf
+def get_pdf_from_url(url):
+    headers = {'User-Agent': 'Mozilla/5.0 (X11; Windows; Windows x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.5060.114 Safari/537.36'}
+    response = requests.get(url=url, headers=headers, timeout=120)
+    # Save the response content to a file
+    with open('document.pdf', 'wb') as file:
+        file.write(response.content)
+    return 'document.pdf'
+
+# abstracted from https://github.com/alejandro-ao/langchain-ask-pdf
+def questiondocument(user_question, url):
+    load_dotenv()
+    document = get_pdf_from_url(url)
+    pdf_reader = PdfReader(document)
+    text = ""
+    for page in pdf_reader.pages:
+      text += page.extract_text()
+        
+    # chunking
+    text_splitter = CharacterTextSplitter(
+      separator="\n",
+      chunk_size=1000,
+      chunk_overlap=200,
+      length_function=len
+    )
+    chunks = text_splitter.split_text(text)
+      
+    # embeddings
+    embeddings = OpenAIEmbeddings()
+    knowledge_base = FAISS.from_texts(chunks, embeddings)
+      
+    docs = knowledge_base.similarity_search(user_question)
+        
+    llm = OpenAI()
+    chain = load_qa_chain(llm, chain_type="stuff")
+    with get_openai_callback() as cb:
+      response = chain.run(input_documents=docs, question=user_question)
+
+    cost = cb
+    return response, cost
+
+demo = gr.Interface(
+    fn=questiondocument,
+    inputs=["text", "text"],
+    outputs=["text","text"],
+)
+demo.launch(share=True)
